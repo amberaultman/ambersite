a37bb66baf4d0e1909562dd6860e0d8e57ea5662
Amber Aultman <amber.aultman@gmail.com>
2014-05-10 18:20:39 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ambersite::Application.config.secret_key_base = '129b2c1561d6d08a1f4cda662cb7124cf63cc949f9810fa13bb6aaafb918a7013bf79f7819dbffe91f3e093bb941a6a04f19147cb6f283403ce3b45daed33ead'
